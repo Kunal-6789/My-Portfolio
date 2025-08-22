ae771a81e7741fd76fd32c9d4b5e03766d62d8cc
unknown <kunals030204@gmail.com>
2025-08-23 03:55:12 +0530

    Navbar issue

--- src/sections/Nav.jsx
+++ src/sections/Nav.jsx
@@ -31,7 +31,7 @@ function Navigation() {
     </ul>
   );
 }
-const Navbar = () => {
+const Nav = () => {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="fixed inset-x-0 z-20 w-full backdrop-blur-lg bg-primary/40">
@@ -75,4 +75,4 @@ const Navbar = () => {
   );
 };
 
-export default Navbar;
+export default Nav;
